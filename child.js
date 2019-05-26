adfec393af29abfee86919c4e03275ffe280cdee
nadir <nadirlaskar@gmail.com>
2019-05-27 02:25:40 +0530

    added fake socket

--- child.js
+++ child.js
@@ -3,11 +3,11 @@ var port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
-    msg("server", server);
+    start("server", server);
 });
 
 
-const msg = (m, server) => {
+const start = (m, server) => {
     if (m === 'server') {
         const io = require('socket.io')();
         io.serveClient(false);
@@ -16,75 +16,36 @@ const msg = (m, server) => {
     }
 }
 
-process.on('message', msg);
-
 const setup = (io) => {
-    // Chatroom
-
-    var numUsers = 0;
-
     io.on('connection', (socket) => {
-        var addedUser = false;
-        socket.use((packet,next)=>{
+        process.send({ connect: socket.id });
+        socket.use((packet, next) => {
             process.send({
                 socket: socket.id,
                 event: packet[0],
                 data: packet[1]
             })
             next();
+        });
+
+        ["disconnect"].forEach(e => { //add other system event to proxy
+            socket.on(e, () => {
+                process.send({
+                    socket: socket.id,
+                    event: e,
+                    data: e
+                })
+            })
         })
-        // when the client emits 'new message', this listens and executes
-        socket.on('new message', (data) => {
-            // we tell the client to execute 'new message'
-            socket.broadcast.emit('new message', {
-                username: socket.username,
-                message: data
-            });
-        });
-
-        // when the client emits 'add user', this listens and executes
-        socket.on('add user', (username) => {
-            if (addedUser) return;
-
-            // we store the username in the socket session for this client
-            socket.username = username;
-            ++numUsers;
-            addedUser = true;
-            socket.emit('login', {
-                numUsers: numUsers
-            });
-            // echo globally (all clients) that a person has connected
-            socket.broadcast.emit('user joined', {
-                username: socket.username,
-                numUsers: numUsers
-            });
-        });
-
-        // when the client emits 'typing', we broadcast it to others
-        socket.on('typing', () => {
-            socket.broadcast.emit('typing', {
-                username: socket.username
-            });
-        });
-
-        // when the client emits 'stop typing', we broadcast it to others
-        socket.on('stop typing', () => {
-            socket.broadcast.emit('stop typing', {
-                username: socket.username
-            });
-        });
-
-        // when the user disconnects.. perform this
-        socket.on('disconnect', () => {
-            if (addedUser) {
-                --numUsers;
-
-                // echo globally that this client has left
-                socket.broadcast.emit('user left', {
-                    username: socket.username,
-                    numUsers: numUsers
-                });
-            }
-        });
     });
+
+
+    process.on("message", ({ name, message, broadcast, socket }) => {
+        if (broadcast) {
+            io.sockets.connected[socket]
+                ? io.sockets.connected[socket].broadcast.emit(name, message)
+                : io.sockets.emit(name, message);
+        }
+        else io.sockets.connected[socket].emit(name, message);
+    })
 }
---
+++ fake.socket.io.js
@@ -0,0 +1,28 @@
+
+function Socket(socket, subprocess) {
+    this.map = {};
+    this.on = (name, callback) => { this.map[name] ? this.map[name].push(callback) : this.map[name] = [callback]; };
+    this.emit = (name, message, broadcast = false) => {
+        subprocess.send({ name, message, broadcast, socket })
+    }
+    this.broadcast = { emit: (name, message) => this.emit(name, message, true) };
+}
+
+Socket.prototype.fire = function (event, data) {
+    this.map[event].forEach(cb => cb(data));
+}
+
+const socketMap = {};
+module.exports = function (subprocess) {
+    this.map = {};
+    this.on = (name, callback) => { this.map[name] ? this.map[name].push(callback) : this.map[name] = [callback]; };
+    subprocess.on("message", (data) => {
+        console.log(data);
+        if (data.connect) {
+            socketMap[data.connect] = new Socket(data.connect, subprocess);
+            this.map["connection"].forEach(cb => cb(socketMap[data.connect]));
+        } else {
+            socketMap[data.socket].fire(data.event, data.data);
+        }
+    });
+}
