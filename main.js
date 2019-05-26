de32d47e43c4535af1c9a582408bda6e83adfb94
nadir <nadirlaskar@gmail.com>
2019-05-27 00:52:07 +0530

    impl message passing between two process

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const subprocess = require('child_process').fork('child.js',[],{execArgv:["--inspect-brk"]});
+const subprocess = require('child_process').fork('child.js', [], { execArgv: ["--inspect-brk"] });
 
-// Open up the server object and send the handle.
-const server = require('http').createServer();
-server.listen(8080, () => {
-    console.log("listening on 1337");
-    subprocess.send('server', server);
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
 });
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+subprocess.on("message",(data)=>{
+  console.log(data);
+})
